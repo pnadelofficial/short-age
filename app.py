de40645caf876aed0c0781fee15201b81368bacc
pnadelofficial <75741790+pnadelofficial@users.noreply.github.com>
2025-06-29 21:12:19 +0000

    instructions

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import streamlit as st
 st.title("Short Age")
 st.subheader("A Height Game")
 
+with st.expander(expanded=True):
+    st.markdown("""
+The object of this game is to find a celebrity who has the same age as their height. For example, if you guess an actor is 55 and 5ft 5 then you would win. Otherwise... well... you're a loser. Select a celeb below to get started. 
+    """)
+
 @st.cache_data
 def read_data():
     import pandas as pd
